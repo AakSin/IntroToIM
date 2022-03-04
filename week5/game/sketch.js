63feed12373a4a6d05fe60a6c52d1564fbf7913b
AakSin <29751759+AakSin@users.noreply.github.com>
2022-03-04 19:18:08 +0400

    start scene created, game scene variable set up

--- week5/game/sketch.js
+++ week5/game/sketch.js
@@ -6,10 +6,11 @@ function preload() {
   sound = loadSound("../assets/07. Pocky Boy.mp3");
 }
 let healthBarFull;
+let cnv;
 function setup() {
   noStroke();
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.mouseClicked(togglePlay);
+  cnv = createCanvas(windowWidth, windowHeight);
+
   fft = new p5.FFT(0.8, 256);
   frameRate(60);
   character = new Character();
@@ -21,75 +22,85 @@ function setup() {
 let oldSum = 0;
 let newSum = 0;
 let oldFc = -60;
+let gameScene = 0;
 function draw() {
   fill("white");
-  newSum = 0;
-  background(220);
-  character.draw();
-  character.move();
+  switch (gameScene) {
+    case 0:
+      fill(0, 102, 153);
+      text("click to start", 100, 100);
+      cnv.mouseClicked(togglePlay);
+      break;
+    case 1:
+      newSum = 0;
+      background(220);
+      character.draw();
+      character.move();
 
-  if (sound.isPlaying()) {
-    let spectrum = fft.analyze();
-    if (
-      sound.currentTime() < 73 ||
-      (sound.currentTime() >= 103 && sound.currentTime() < 133)
-    ) {
-      for (let i = 60; i < 100; i++) {
-        newSum += spectrum[i];
-      }
-      if (newSum - oldSum > 400 && frameCount - oldFc > 30) {
-        let planet = new Planet();
-        planetArray.push(planet);
-        print("spawn");
-        oldFc = frameCount;
-      }
-    } else if (
-      (sound.currentTime() >= 73 && sound.currentTime() < 103) ||
-      sound.currentTime() >= 132
-    ) {
-      for (let i = 0; i < 10; i++) {
-        newSum += spectrum[i];
-      }
-      // console.log(newSum - oldSum);
-      if (newSum - oldSum > 25 && frameCount - oldFc > 30) {
-        let planet = new Planet();
-        planetArray.push(planet);
-        print("spawn");
-        oldFc = frameCount;
-      }
-    }
+      if (sound.isPlaying()) {
+        let spectrum = fft.analyze();
+        if (
+          sound.currentTime() < 73 ||
+          (sound.currentTime() >= 103 && sound.currentTime() < 133)
+        ) {
+          for (let i = 60; i < 100; i++) {
+            newSum += spectrum[i];
+          }
+          if (newSum - oldSum > 400 && frameCount - oldFc > 30) {
+            let planet = new Planet();
+            planetArray.push(planet);
+            print("spawn");
+            oldFc = frameCount;
+          }
+        } else if (
+          (sound.currentTime() >= 73 && sound.currentTime() < 103) ||
+          sound.currentTime() >= 132
+        ) {
+          for (let i = 0; i < 10; i++) {
+            newSum += spectrum[i];
+          }
+          // console.log(newSum - oldSum);
+          if (newSum - oldSum > 25 && frameCount - oldFc > 30) {
+            let planet = new Planet();
+            planetArray.push(planet);
+            print("spawn");
+            oldFc = frameCount;
+          }
+        }
 
-    oldSum = newSum;
-  }
-
-  for (let i = planetArray.length - 1; i >= 0; i--) {
-    planetArray[i].draw();
-    planetArray[i].move();
-    if (planetArray[i].x < -100) {
-      planetArray.splice(i, 1);
-    } else {
-      if (
-        abs(character.x - planetArray[i].x) <=
-          planetArray[i].r + character.rX &&
-        abs(character.y - planetArray[i].y) <= planetArray[i].r + character.rY
-      ) {
-        character.health -= 0.5;
+        oldSum = newSum;
       }
-    }
-  }
 
-  let healthBarRed = character.health;
-  fill("black");
-  rect(width - healthBarFull - 50, 40, healthBarFull, 10);
-  fill("red");
-  rect(
-    width - healthBarFull - 50 - (healthBarFull - healthBarRed),
-    40,
-    healthBarRed,
-    10
-  );
-  if (healthBarRed == 0) {
-    alert("Stop");
+      for (let i = planetArray.length - 1; i >= 0; i--) {
+        planetArray[i].draw();
+        planetArray[i].move();
+        if (planetArray[i].x < -100) {
+          planetArray.splice(i, 1);
+        } else {
+          if (
+            abs(character.x - planetArray[i].x) <=
+              planetArray[i].r + character.rX &&
+            abs(character.y - planetArray[i].y) <=
+              planetArray[i].r + character.rY
+          ) {
+            character.health -= 0.5;
+          }
+        }
+      }
+
+      let healthBarRed = character.health;
+      fill("black");
+      rect(width - healthBarFull - 50, 40, healthBarFull, 10);
+      fill("red");
+      rect(
+        width - healthBarFull - 50 - (healthBarFull - healthBarRed),
+        40,
+        healthBarRed,
+        10
+      );
+      if (healthBarRed == 0) {
+        alert("Stop");
+      }
   }
 }
 
@@ -101,6 +112,7 @@ function togglePlay() {
     amplitude = new p5.Amplitude();
     amplitude.setInput(sound);
   }
+  gameScene = 1;
 }
 
 class Planet {
